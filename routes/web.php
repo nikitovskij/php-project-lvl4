f268071cff4f3bf33cb35251a1c789a0874b31ca
nikitovskij <nbaranovsky24@gmail.com>
2021-03-08 00:08:53 +0100

    testing rollbar

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,5 @@ Route::get('/', function () {
 });
 
 Route::get('/rollbar', function () {
-    return \Log::debug('Test debug message');
+    return \Rollbar\Rollbar::log('debug', \Log::debug('Test debug message: rollbar'));
 });
