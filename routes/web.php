89ce2397f7e3c20c42429598236e418e1ead0dee
nikitovskij <nbaranovsky24@gmail.com>
2021-03-11 21:38:25 +0100

    testing rollbar

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    \Log::debug('Test debug message');
     return view('welcome');
+});
+
+Route::get('/rollbar', function () {
+    \Log::debug('Test debug message');
 });
