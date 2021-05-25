752d639cae505c5e214a4cb8462a7ddd42237672
nikitovskij <nbaranovsky24@gmail.com>
2021-05-25 13:09:23 +0200

    fix validation type

--- app/Http/Requests/StoreTaskRequest.php
+++ app/Http/Requests/StoreTaskRequest.php
@@ -31,7 +31,7 @@ class StoreTaskRequest extends FormRequest
             'status_id' => 'required|exists:task_statuses,id',
             'assigned_to_id' => 'nullable|exists:users,id',
             'labels' => 'nullable|array',
-            'labels.*' => 'nullable|int|distinct',
+            'labels.*' => 'nullable|integer|distinct',
         ];
     }
 }
