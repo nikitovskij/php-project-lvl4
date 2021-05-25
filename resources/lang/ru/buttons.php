80a5abd9bdde8b4abed50df2e15014bb18fd4d50
nikitovskij <nbaranovsky24@gmail.com>
2021-05-25 16:43:18 +0200

    fix localization

--- resources/lang/ru/buttons.php
+++ resources/lang/ru/buttons.php
@@ -15,4 +15,13 @@ return [
     'edit' => 'Изменить',
     'delete' => 'Удалить',
     'update' => 'Обновить',
+    'login' => 'Вход',
+    'registration' => 'Регистрация',
+    'sign-in' => 'Войти',
+    'logout' => 'Выход',
+    'check-in' => 'Зарегистрировать',
+    'reset-password' => 'Сброс пароля',
+    'confirm-password' => 'Подтверждение',
+    'remember-me' => 'Запомнить меня',
+    'forgot-password' => 'Забыли пароль?'
 ];
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,13 +51,13 @@
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('buttons.login') }}</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('buttons.registration') }}</a>
                                 </li>
                             @endif
                         @else
@@ -70,7 +70,7 @@
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        {{ __('buttons.logout') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
